4b59070d499fab5219711cf9ea4229367c67ce6d
Your Name <alaaahmedsaber2002@gmail.com>
2024-09-09 23:27:59 +0300

    restyle header

--- my-app/src/app/components/Navbar.tsx
+++ my-app/src/app/components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
         <header className="mb-8 border-b">
             <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
                 <Link href="/">
-                    <h1 className="text-2xl md:text-4xl font-bold">
+                    <h1 className="text-2xl md:text-3xl font-bold">
                         Next<span className="text-primary">Commerce</span>
                     </h1>
                 </Link>
@@ -46,7 +46,7 @@ export default function Navbar() {
                 <div className="flex divide-x border-r sm:border-l">
                     <Button
                         variant={"outline"}
-                        className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
+                        className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 rounded-none"
                     >
                         <ShoppingBag />
                         <span className="hidden text-xs font-semibold text-gray-500 sm:block">
