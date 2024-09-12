d7c1c45d016f8ba1447bdd1f19b4afb9b8aef593
Your Name <alaaahmedsaber2002@gmail.com>
2024-09-12 04:09:41 +0300

    edit query for fetch newest products

--- my-app/src/app/components/Newest.tsx
+++ my-app/src/app/components/Newest.tsx
@@ -6,13 +6,14 @@ import { Button } from "@/components/ui/button"
 import ProductsCards from "./ProductsCards"
 
 async function getProduct(){
-    const query = `*[_type == 'product'][0...4] | order(_cteatedAt desc){
-        _id,
-        price,
-        name,
-        "slug": slug.current,
-        "categoryName": category->name,
-        "imageUrl": images[0].asset->url
+    
+    const query = `*[_type == 'product'][0...4] | order(_createdAt desc){
+    _id,
+    price,
+    name,
+    "slug": slug.current,
+    "categoryName": category->name,
+    "imageUrl": images[0].asset->url
 }`
 
     const data = await client.fetch(query)
